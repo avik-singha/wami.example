b00e68619297d0f93f9d038eb0047e058afa1512
imcgraw <imcgraw@localhost>
2009-12-10 12:42:38 -0500

    Example Wami server.

---
+++ src/edu/mit/csail/sls/wami/MyWamiApplication.java
@@ -0,0 +1,12 @@
+package edu.mit.csail.sls.wami;
+
+import edu.mit.csail.sls.wami.jsapi.ClientControlledApplication;
+
+/**
+ * Override methods in this class to alter server-side functionality.
+ * 
+ * @author imcgraw
+ */
+public class MyWamiApplication extends ClientControlledApplication {
+
+}
